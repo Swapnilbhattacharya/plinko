4eee76ce2a0ffa57c4a5a61ea7455392edf63b8a
Swapnilbhattacharya <75618173+Swapnilbhattacharya@users.noreply.github.com>
2021-07-16 21:49:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+const Body=Matter.Body;
+
+var divisions=[];
+var divisionHeight=[];
+var particles=[];
+var plinkos=[];
+function preload(){
+
+}
+
+function setup(){
+    createCanvas(800,700);
+    engine=Engine.create;
+    world=engine.world;
+    
+    //create the bodies here
+    ground=new Ground(400,690,800,20);
+
+    for(var i = 0; i<=width; i= i+80){
+      divisions.push(new Division(i,height-divisionHeight/2,10,divisionHeight));
+    }
+    
+    for(var j= 75; j <=width; j= j+50){
+        plinkos.push(new Plinko(j,75));
+    }
+
+    for(var j=50; j<=width-10 ; j=j+50){
+        plinkos.push(new Plinko(j,175));
+    }
+    
+    for(var j=75; j<=width; j=j+50){
+        plinkos.push(new Plinko(j,275));
+    }
+    
+    for(var j=50; j<=width-10; j=j+50){
+        plinkos.push(new Plinko(j,375));
+    }
+    
+    Engine.run(engine);
+}
+
+function draw(){
+    rectMode(CENTER);
+    background(0);
+
+    drawSprites();
+
+    for(var n=0; n<divisions.length; n++){
+        divisions[n].display();
+    }    
+
+    for(frameCount % 60 ===0) {
+
+    }
+    
+    
+    
+   
+
+
+}
